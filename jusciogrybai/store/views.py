6c08769489eafe3b0b9ca166159b12c042f05ec5
Gambit9111 <bitgam911@gmail.com>
2022-09-06 00:01:41 +0000

    Add/remove to cart feature is done

--- jusciogrybai/store/views.py
+++ jusciogrybai/store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from re import M
+from django.shortcuts import render, redirect
 from .models import Category, Product, Order, OrderItem
 
 # categories view
@@ -12,25 +13,55 @@ def products_by_category(request, slug):
 
 def product_detail(request, slug_cat, slug_prod):
     product = Product.objects.get(slug=slug_prod)
-
-    if request.method == 'POST':
-        product = Product.objects.get(slug=slug_prod)
-        device = request.COOKIES['device']
-        print(request.POST)
-        print(device)
-        order, created = Order.objects.get_or_create(device=device, complete=False)
-        print(order)
-        orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-        orderItem.quantity=request.POST['quantity']
-        orderItem.save()
+    device = request.COOKIES['device']
+    try:
         order_item = OrderItem.objects.get(product=product)
-        context = { 'product': product, 'order_item': order_item }
-    else:
-        try:
+        if request.method == 'POST':
+            order, created = Order.objects.get_or_create(device=device, complete=False)
+            orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+            orderItem.quantity=request.POST['quantity']
+            orderItem.save()
             order_item = OrderItem.objects.get(product=product)
-            context = { 'product': product, 'order_item': order_item }
-            print(order_item.quantity)
-        except OrderItem.DoesNotExist:
-            context = { 'product': product }
+            return redirect('store:cart_view')
+        else:
+            try:
+                order_item = OrderItem.objects.get(product=product)
+                context = { 'product': product, 'order_item': order_item }
+            except OrderItem.DoesNotExist:
+                context = { 'product': product }
+
+    except OrderItem.DoesNotExist:
+        if request.method == 'POST':
+            order, created = Order.objects.get_or_create(device=device, complete=False)
+            orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+            orderItem.quantity=request.POST['quantity']
+            orderItem.save()
+            order_item = OrderItem.objects.get(product=product)
+            return redirect('store:products_by_category', slug=slug_cat)
+        else:
+            try:
+                order_item = OrderItem.objects.get(product=product)
+                context = { 'product': product, 'order_item': order_item }
+            except OrderItem.DoesNotExist:
+                context = { 'product': product }
 
     return render(request, "store/product_detail.html", context)
+
+
+
+def cart_view(request):
+    order = Order.objects.get(device=request.COOKIES['device'], complete=False)
+    order_items = OrderItem.objects.filter(order=order)
+
+    if request.method == "POST":
+        product=request.POST['product_id']
+        order_item = OrderItem.objects.get(product=product)
+        order_item.delete()
+        
+    if order_items:
+        pass
+    else:
+        order.delete()
+        return redirect('store:all_categories')
+
+    return render(request, "store/cart.html", {"order_items": order_items})
